f32a5d72d0ae379b36c7dc4f737ad4ed3907f704
HM Nayem <hmnayem@stacklearner.com>
2021-01-15 03:45:31 +0600

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,42 @@
 module.exports = {
-  siteMetadata: {
-    title: "web-boost",
-  },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+	siteMetadata: {
+		title: 'Web Boost - Follow Agile to Learn',
+	},
+	plugins: [
+		'gatsby-plugin-styled-components',
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				trackingId: 'G-RVMNCQBRFE',
+			},
+		},
+		'gatsby-plugin-sharp',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sitemap',
+		'gatsby-plugin-offline',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png',
+			},
+		},
+		'gatsby-plugin-mdx',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/',
+			},
+			__key: 'pages',
+		},
+	],
 };
