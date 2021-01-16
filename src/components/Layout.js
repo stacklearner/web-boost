298c3f8f1969976ba2308f1769b735d0090287a3
HM Nayem <cats.twinkle@gmail.com>
2021-01-17 00:40:28 +0600

    Facebook messenger enabled

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,22 @@
 /** @jsx jsx */
 import { useColorMode, jsx } from 'theme-ui';
 import '../static/css/font.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 import Navbar from './Navbar';
 import SEO from './SEO';
 
 const Layout = ({ children }) => {
 	const [colorMode, setColorMode] = useColorMode();
-
 	return (
 		<div>
 			<SEO />
 			<Navbar mode={colorMode} setMode={setColorMode} />
 			{children}
+			<MessengerCustomerChat
+				pageId='101277768070549'
+				appId='3434413739996523'
+			/>
 		</div>
 	);
 };
